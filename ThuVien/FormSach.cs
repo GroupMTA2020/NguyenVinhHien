139ff538d93ec706b29ec0344257e6163d3d1e8a
NguyenQuocKhanh <44228638+kaanblack09@users.noreply.github.com>
2020-09-29 22:29:11 +0700

    1
    thêm chức năng thêm, sửa , xóa , lưu , phân quyền

--- ThuVien/FormSach.cs
+++ ThuVien/FormSach.cs
@@ -147,32 +147,113 @@ namespace ThuVien
 
         private void Add_Click(object sender, EventArgs e)
         {
+            Save_btn.Tag = "ADD";
+            txtmasach.Text = "MS" + dtgvSach.Rows.Count.ToString("0000");
+            btnReload();
+            textBoxChangeState();
         }
-        
+        public void textBoxChangeState()
+        {
+            txtmasach.Enabled = txttensach.Enabled = txttacgia.Enabled
+                = txttheloai.Enabled = txtnhaxuatban.Enabled = txtgiasach.Enabled
+                = nb_SoLuong.Enabled = txttinhtrang.Enabled = !txtmasach.Enabled;
+        }
+        void btnReload()
+        {
+            Repair_btn.Visible = Remove_btn.Visible =
+                Add_btn.Visible = !Add_btn.Visible;
+            Save_btn.Visible = Cancel_btn.Visible = !Save_btn.Visible;
+        }
         private void Save_Click(object sender, EventArgs e)
         {
-            
+            if (Save_btn.Tag.ToString() == "ADD")
+            {
+                Sach s = new Sach(txtmasach.Text, txttensach.Text, txttacgia.Text, txttheloai.Text
+                                , txtnhaxuatban.Text, Convert.ToInt32(txtgiasach.Text), Convert.ToInt32(nb_SoLuong.Value), txttinhtrang.Text);
+                var i = s.insertSach();
+                if (i == 0)
+                {
+                    MessageBox.Show("Add not success.");
+                }
+                else
+                {
+                    MessageBox.Show("Add success.");
+                    showTableSach();
+                }
+                btnReload();
+                textBoxChangeState();
+            }
+            else if (Save_btn.Tag.ToString() == "Repair")
+            {
+                Sach s = new Sach(txtmasach.Text, txttensach.Text, txttacgia.Text, txttheloai.Text
+                                , txtnhaxuatban.Text, Convert.ToInt32(txtgiasach.Text), Convert.ToInt32(nb_SoLuong.Value), txttinhtrang.Text);
+                int i = s.updateBook();
+                if (i == 0)
+                    MessageBox.Show("update not success.");
+                else
+                {
+                    MessageBox.Show("update success.");
+                    showTableSach();
+                }
+                txtmasach.Enabled = !txtmasach.Enabled;
+                btnReload();
+                textBoxChangeState();
+            }
         }
 
         private void Repair_Click(object sender, EventArgs e)
         {
-           
+            Save_btn.Tag = "Repair";
+            btnReload();
+            textBoxChangeState();
+            txtmasach.Enabled = !txtmasach.Enabled;
         }
 
         private void Cancel_Click(object sender, EventArgs e)
         {
-           
+            btnReload();
+            textBoxChangeState();
         }
 
         private void Remove_Click(object sender, EventArgs e)
         {
-            
+            if (txtmasach.Text != "")
+            {
+                Sach s = new Sach(txtmasach.Text);
+                var i = s.deleteBook();
+                if (i > 0)
+                {
+                    showTableSach();
+                }
+                else MessageBox.Show("Delete Not Success");
+            }
+            else { MessageBox.Show("Choose a Row to Remove."); }
         }
-        
+        void design()
+        {
+            dtgvSach.BorderStyle = BorderStyle.None;
+            dtgvSach.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            dtgvSach.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dtgvSach.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            dtgvSach.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            dtgvSach.BackgroundColor = Color.White;
+            dtgvSach.EnableHeadersVisualStyles = false;
+            dtgvSach.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dtgvSach.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            dtgvSach.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dtgvSach.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
 
         private void FormSach_Load(object sender, EventArgs e)
         {
-           
+            if (Taikhoan.Quyen == Quyentruy.User)
+            {
+                Add_btn.Visible = false;
+                Repair_btn.Visible = false;
+                Save_btn.Visible = false;
+                Cancel_btn.Visible = false;
+                Remove_btn.Visible = false;
+            }
         }
     }
 }
